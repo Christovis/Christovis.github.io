96e4bc95ef75a28af3828605d860c49708e1797b
christovis <chrbecker01@gmail.com>
2022-07-08 23:48:13 +0200

    update build

--- build_instructions.sh
+++ build_instructions.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
 
 # from my server space chrbecker.net on greenhost.nl server type into shell:
 wget -r -p -U Mozilla --wait=1 --limit-rate=35K https://christovis.github.io//
+
+# replace all references to github page with link to my website
+sed "s/christovis.github.io/chrbecker.net/g" ./talks/* -i
+sed "s/christovis.github.io/chrbecker.net/g" ./research/* -i
+sed "s/christovis.github.io/chrbecker.net/g" ./blog/* -i
+sed "s/christovis.github.io/chrbecker.net/g" ./software -i
+sed "s/christovis.github.io/chrbecker.net/g" ./contact -i
+sed "s/christovis.github.io/chrbecker.net/g" ./about -i
+sed "s/christovis.github.io/chrbecker.net/g" ./index.html -i
+sed "s/christovis.github.io/chrbecker.net/g" ./feed.xml -i
+sed "s/christovis.github.io/chrbecker.net/g" ./robots.txt -i
